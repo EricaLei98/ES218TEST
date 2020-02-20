993f392802fba58c224d1bd9850aae55d3db117c
EricaLei98 <zhijun_0705@126.com>
2020-02-19 23:26:30 -0500

    second commit

--- feb20.r
+++ feb20.r
@@ -1,8 +1,8 @@
-# the second edit, add something above
-library(Stat2Data)
-
 # added first line of code
 library(tidyr)
 
 # Now I want to see a dataset
 cars
+
+# Second round of edit, I deleted the first two lines
+hist(cars$speed)
